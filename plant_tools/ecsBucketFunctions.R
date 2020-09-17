e417a538132b591a45078ac5844851f2b6b2f8fb
schafnoir <cmeier47@gmail.com>
2020-09-17 14:27:42 -0600

    Updated ECS bucket get/put functions and added example workflow using functions

--- plant_tools/ecsBucketFunctions.R
+++ plant_tools/ecsBucketFunctions.R
@@ -3,11 +3,11 @@
 
 ##  Function to get DHPs from ECS bucket, then save DHPs in a user-defined location. 
 ##  For each image, the function re-creates the directory structure defined by the value in the ECS Key field.
-get_file_ecs <- function(objectKey, volumePath, ecsBucket, ecsUrl){
-  # [MODIFY] Get original filePath from ECS Key -> string indices below are specific to 12 char DHP filename
-  # [MODIFY] A function other than str_sub may be more appropriate if file name is variable
-  filePath <- stringr::str_sub(objectKey, start = 1, end = -13)
-  
+get_file_ecs <- function(objectKey, volumePath, ecsBucket){
+  # Get original filePath from ECS Key
+  filePath <- stringr::str_split(string = objectKey, pattern = "/")
+  filePath <- paste(filePath[[1]][1:I(lengths(filePath)-1)], collapse = "/")
+    
   # Create directory from filePath if it does not exist
   if(!file.exists(paste(volumePath, filePath, sep = "/"))){
     dir.create(paste(volumePath, filePath, sep = "/"), recursive = TRUE)
@@ -19,7 +19,7 @@ get_file_ecs <- function(objectKey, volumePath, ecsBucket, ecsUrl){
   } else {
     # Save object to volumePath with original directory structure specified by Key
     aws.s3::save_object(object = objectKey, bucket = ecsBucket, file = paste(volumePath, objectKey, sep = "/"),
-                        check_region = FALSE, base_url = ecsUrl)
+                        check_region = FALSE)
     # Report progress -> use with 'pbapply' package to report progress in real-time for long input lists
     print(paste("File saved to specified volumePath:", objectKey, sep = " "), quote = FALSE)
   }
@@ -29,14 +29,14 @@ get_file_ecs <- function(objectKey, volumePath, ecsBucket, ecsUrl){
 
 ##  Function to put a file into specified ECS bucket and define the ECS object Key for the file
 ##  Function checks to determine whether file already exists with specified object Key
-put_file_ecs <- function(filePath, objectKey, ecsBucket, ecsUrl){
+put_file_ecs <- function(sourceFile, objectKey, ecsBucket){
   # Determine whether file at filePath with specified objectKey exists in the ECS bucket
-  if(aws.s3::head_object(object = objectKey, bucket = ecsBucket, base_url = ecsUrl, check_region = FALSE)){
+  if(aws.s3::head_object(object = objectKey, bucket = ecsBucket, check_region = FALSE)){
     print(paste0("Object already exists in ", ecsBucket, ": ", objectKey), quote = FALSE)
   } else {
     # Put object into bucket with specified objectKey, and report successful transfer
-    aws.s3::put_object(file = filePath, object = objectKey, bucket = ecsBucket, 
-                       acl = "public-read", base_url = ecsUrl, check_region = FALSE)
+    aws.s3::put_object(file = sourceFile, object = objectKey, bucket = ecsBucket, 
+                       acl = "public-read", check_region = FALSE)
     print(paste0("Successful put to ", ecsBucket, ": ", objectKey), quote = FALSE)
   }
 }
@@ -45,18 +45,18 @@ put_file_ecs <- function(filePath, objectKey, ecsBucket, ecsUrl){
 
 ##  Function to copy an existing object in specified ECS bucket to new object with new specified object Key in same bucket
 ##  Function checks to determine original object exists, and whether new object Key exists before copying
-copy_file_ecs <- function(objectKey, newObjectKey, ecsBucket, ecsUrl){
+copy_file_ecs <- function(objectKey, newObjectKey, ecsBucket){
   # Determine whether objectKey is in bucket
-  if(!aws.s3::head_object(object = objectKey, bucket = ecsBucket, base_url = ecsUrl, check_region = FALSE)){
+  if(!aws.s3::head_object(object = objectKey, bucket = ecsBucket, check_region = FALSE)){
     print(paste0("Object not in specified bucket: ", objectKey), quote = FALSE)
   
   # Determine whether newObjectKey already exists in bucket
-  } else if(aws.s3::head_object(object = newObjectKey, bucket = ecsBucket, base_url = ecsUrl, check_region = FALSE)){
+  } else if(aws.s3::head_object(object = newObjectKey, bucket = ecsBucket, check_region = FALSE)){
     print(paste0("New objectKey already exists in bucket: ", newObjectKey), quote = FALSE)
   } else {
     # Copy objectKey to newObjectKey in the bucket, and status result
     aws.s3::copy_object(from_object = objectKey, to_object = newObjectKey, from_bucket = ecsBucket, to_bucket = ecsBucket,
-                        acl = "public-read", base_url = ecsUrl, check_region = FALSE)
+                        acl = "public-read", check_region = FALSE)
     print(paste0("Successful copy: ", objectKey, " to ", newObjectKey), quote = FALSE)
   }
 }
---
+++ plant_tools/ecsBucketReadWrite.R
@@ -0,0 +1,59 @@
+### Use ECS Bucket Functions to get and put files from and to an ECS bucket
+
+### Setup ####
+#   Load required libraries
+library(aws.s3)
+library(dplyr)
+library(pbapply)
+library(stringr)
+
+#   Load ECS Keys, Secrets, and Functions
+if(file.exists("/Users/Pajaro")){
+  source("~/Box/computing/ecsBucketKeys.R")
+  funPath <- "~/Documents/workDocuments/gitRepositories/neon-plant-sampling/plant_tools"
+  source(paste(funPath, "ecsBucketFunctions.R", sep = "/"))
+}
+
+#   Set System Environment to authenticate to AWS
+Sys.setenv("AWS_ACCESS_KEY_ID" = micKey, "AWS_SECRET_ACCESS_KEY" = micSecret,
+           "AWS_S3_ENDPOINT" = "neonscience.org", "AWS_DEFAULT_REGION" = "s3.data")
+
+
+
+### Read ECS bucket contents ####
+#   --> change 'max = 100' to 'max = Inf' to retrieve listing of complete bucket contents
+temp <- aws.s3::get_bucket_df(bucket='neon-microbial-raw-seq-files', check_region = FALSE, max = 10)
+temp <- temp %>% filter(Size != 0)
+
+
+
+### Get files from ECS bucket ####
+#   Define bucket and path to where files will be written
+bucket <- 'neon-microbial-raw-seq-files'
+writePath <- '~/Desktop'
+
+#   Example with single file
+get_file_ecs(objectKey = temp$Key[1], volumePath = "~/Desktop", ecsBucket = bucket)
+
+#   Example with file list and pblapply
+pbapply::pblapply(X = temp$Key, FUN = get_file_ecs,
+                 ecsBucket = bucket, volumePath = writePath)
+
+
+
+### Put files to ECS bucket ####
+##  Example of reading source files and defining objectKey values
+#   Define bucket to which source files are put and path for listing source files
+bucket <- 'neon-microbial-raw-seq-files'
+volumePath <- "~/Desktop"
+
+#   Create list of source files
+putFiles <- list.files(path = volumePath, full.names = TRUE, recursive = TRUE)
+putFiles <- data_frame(putFiles) %>% rename(filePaths = putFiles)
+
+#   Create objectKey values from source file path information --> this needs customizing
+putFiles$keys <- putFiles$filePaths
+
+#   Define list of paths, keys, and MoreArgs needed for function
+mapply(FUN = put_file_ecs, putFiles$filePaths, putFiles$keys,
+       MoreArgs = list(ecsBucket = bucket))
