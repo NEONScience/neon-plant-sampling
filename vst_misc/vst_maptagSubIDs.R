0e54c8da643c862164c12148b7d00cffb0ccd2e4
Courtney Meier <cmeier@BattelleEcology.org>
2024-08-06 18:44:30 -0600

    Finding M&T individuals with incorrect subplotID

--- vst_misc/vst_maptagSubIDs.R
+++ vst_misc/vst_maptagSubIDs.R
@@ -1,21 +1,56 @@
-### Find individualIDs in VST plots with incorrect subplotIDs - i.e., those inconsistent with plotType from TOS Spatial Data
+### Find individualIDs in VST: Mapping and Tagging [PROD] with incorrect subplotIDs 
+### - i.e., those inconsistent with plotType from TOS Spatial Data. No need to correct L0 as subplotID is not
+### published in vst_mappingandtagging
 
-#   Retrieve all VST: Mapping and Tagging [PROD] records
-mtQuery <- URLencode(glue::glue('SELECT _record_id, startdate, plotid, subplotid, nestedsubplotid, individualid, tagid, taxonid, pointid, recordtype, stemazimuth, stemdistance FROM "d0b95d92-3345-4b40-9767-c28ddbbacfae" ORDER BY _record_id', .sep = " "))
+library(dplyr)
+library(neonOSTools)
+
+
+##  Retrieve all VST: Mapping and Tagging [PROD] records
+#   Define MT query
+mtQuery <- URLencode(glue::glue('SELECT _record_id, startdate, plotid, subplotid, nestedsubplotid, individualid, tagid, taxonid, pointid, recordtype, stemazimuth, stemdistance FROM "d0b95d92-3345-4b40-9767-c28ddbbacfae" WHERE', "(cfconlytag IS NULL OR cfconlytag='N') ORDER BY _record_id", .sep = " "))
 
 #   Retrieve Fulcrum data and keep latest
-mtData <- get_fulcrum_pageLim(apiToken = fulcrumToken, 
-                              sql = mtQuery) %>%
+mtData <- neonOSTools::get_fulcrum_data(api_token = Sys.getenv("FULCRUM_TOKEN"), 
+                                        sql = mtQuery) %>%
   dplyr::group_by(individualid) %>%
   dplyr::filter(startdate == max(startdate)) %>%
   dplyr::ungroup()
 
+
+##  Retrieve TOS Spatial Data from Fulcrum
+#   Define query
+spatialQuery <- URLencode(glue::glue('SELECT domainid, siteid, plotid, plottype, plotsize, mixedsite, randomsubplota, randomsubplotb FROM "eb10727b-bb4d-4778-9a74-8ee03465417e" WHERE', "subtype='basePlot' ORDER BY _record_id", .sep = " "))
+
+#   Retrieve TOS Spatial Data
+plotSpatial <- neonOSTools::get_fulcrum_data(api_token = Sys.getenv("FULCRUM_TOKEN"),
+                                             sql = spatialQuery) %>%
+  dplyr::arrange(domainid, 
+                 siteid,
+                 plottype,
+                 plotid) %>%
+  dplyr::mutate(plottype = case_when(plottype == "distributed" ~ "distributed",
+                                     plottype == "tower" & mixedsite == TRUE ~ "smTower",
+                                     plottype == "tower" & mixedsite == FALSE ~ "lgTower",
+                                     TRUE ~ plottype)) %>%
+  dplyr::rename(domainID = domainid,
+                siteID = siteid,
+                plotID = plotid,
+                plotType = plottype,
+                mixedSite = mixedsite,
+                randomSubplotA = randomsubplota,
+                randomSubplotB = randomsubplotb)
+
+
+##  Identify issues and summarize
 #   Join with plotSpatial
 mtData <- mtData %>%
   dplyr::left_join(plotSpatial %>%
                      dplyr::select(domainID,
                                    plotID,
-                                   plotType),
+                                   plotType,
+                                   randomSubplotA,
+                                   randomSubplotB),
                    by = c("plotid" = "plotID"))
 
 #   Identify individuals in large-stature Tower plots with incorrect subplotIDs
@@ -38,15 +73,19 @@ smallPlotMT <- mtData %>%
                  subplotid,
                  individualid)
 
-#   Summarize all affected plotIDs
+
+##  Summarize all affected plotIDs
 affectedPlots <- largePlotMT %>%
   dplyr::bind_rows(smallPlotMT) %>%
   dplyr::group_by(domainID,
                   plotid,
-                  plotType) %>%
+                  plotType,
+                  subplotid) %>%
   dplyr::summarise(count = n())
 
-#   Affected individuals
+
+##  Affected individuals
+#   Bind large and small
 affectedIndiv <- largePlotMT %>%
   dplyr::bind_rows(smallPlotMT) %>%
   dplyr::arrange(domainID,
@@ -56,7 +95,67 @@ affectedIndiv <- largePlotMT %>%
   dplyr::relocate(domainID,
                   .before = plotid) %>%
   dplyr::relocate(plotType,
+                  randomSubplotA,
+                  randomSubplotB,
                   .after = plotid)
+
+
+##  Affected plotType and subplotID combos
+affectedCombos <- affectedIndiv %>%
+  dplyr::group_by(plotType,
+                  subplotid) %>%
+  dplyr::summarise(count = n())
+
+
+##  Explore data issues
+temp <- affectedIndiv %>%
+  dplyr::filter(subplotid == "31_100" & plotType == "lgTower")
+#--> All but 3 are from DEJU and have randomSubplotA = "21_400"; map data spot check suggests 21_400 is likely correct
+#--> others can be assigned to randomSubplotA, no other logic makes sense and no info in vst_apparentindividual
+
+temp <- affectedIndiv %>%
+  dplyr::filter(subplotid == "32_100" & plotType == "lgTower")
+#--> most have "23_400" as one of the randomSubplots, a few do not; for those that don't, select randomSubplotA
+
+temp <- affectedIndiv %>%
+  dplyr::filter(subplotid == "40_100" & plotType == "lgTower")
+#--> All are tagOnly records from UKFS_043; most of these do not exist in vst_apparentindividual or have unknown subplotID
+#--> subplotid = "40_100" is illogical, so select randomSubplotA
+
+
+#--> Most of errors are when subplotid == "41_100" (> 8000)
+#   Check whether randomSubplotB == "41_400" when subplotid == "41_100" for these records
+temp <- affectedIndiv %>%
+  dplyr::filter(subplotid == "41_100" & randomSubplotB == "41_400") #--> majority, 7736 records
+#--> For those that are mapped, a spot check revealed that subplotID is correct in vst_apparentindividual, just not in Fulcrum Mapping & Tagging, assign to sublotid = "41_400"
+
+temp <- affectedIndiv %>%
+  dplyr::filter(subplotid == "41_100" & randomSubplotB != "41_400") #--> 305 records
+#--> All are from HARV 2014 (305 records), and majority of these are "tag only" records
+#--> Spot checking multiple records from each plot, none exist in vst_apparentindividual, so select randomSubplotA
+
+temp <- affectedIndiv %>%
+  dplyr::filter(is.na(subplotid) & plotType == "lgTower") #--> 208 records
+#--> Create INC for Field Science to fix? Ignore if individual cannot be found?
+
+  
+##  Create proposed fix
+affectedIndiv <- affectedIndiv %>%
+  dplyr::mutate(fixSubplotID = dplyr::case_when(plotType == "distributed" | plotType == "smTower" ~ "31_400",
+                                                plotType == "lgTower" & subplotid == "31_100" & randomSubplotA == "21_400" ~ "21_400",
+                                                plotType == "lgTower" & subplotid == "31_100" & randomSubplotA != "21_400" ~ randomSubplotA,
+                                                plotType == "lgTower" & subplotid == "32_100" & 
+                                                  (randomSubplotA == "23_400" | randomSubplotB == "23_400") ~ "23_400",
+                                                plotType == "lgTower" & subplotid == "32_100" & randomSubplotA != "23_400" &
+                                                  randomSubplotB != "23_400" ~ randomSubplotA,
+                                                plotType == "lgTower" & subplotid == "40_100" ~ randomSubplotA,
+                                                plotType == "lgTower" & subplotid == "41_100" & randomSubplotB == "41_400" ~ "41_400",
+                                                plotType == "lgTower" & subplotid == "41_100" & randomSubplotB != "41_400" ~ randomSubplotA,
+                                                TRUE ~ NA),
+                .after = subplotid)
+
+
+
 
 
 openxlsx::write.xlsx(affectedPlots, 
