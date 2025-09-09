897bb321cb20562eae59412b3ea6dab9429e6adf
Courtney Meier <cmeier@BattelleEcology.org>
2025-09-09 15:34:16 -0600

    Adding M&T query for latest individualid

--- vst_dendrometer/R_scripts/dendroDev.R
+++ vst_dendrometer/R_scripts/dendroDev.R
@@ -11,6 +11,7 @@ options(useFancyQuotes = FALSE)
 #   Define api-tokens
 dbHost <- "prod-commondb.gcp.neoninternal.org"
 dbPwd <- "nD0BEMA8Cb6efs1dniFkfOIPeBE09HIy"
+fulcrumToken <- Sys.getenv("FULCRUM_TOKEN")
 
 #   Set up vstqaqc db connection pool
 conPool <- pool::dbPool(drv = RPostgres::Postgres(),
@@ -51,4 +52,33 @@ ORDER BY inv.eventid, inv.plotid, inv.individualid;")
 aiDF <- RPostgres::dbGetQuery(conn = conPool,
                               statement = aiQuery)
 
+
+
+### Retrieve M&T data from PostgreSQL data base to associate taxonID, etc. with AI data
+mtQuery <- glue::glue('SELECT m1.date, m1.subplotid, m1.nestedsubplotid, UPPER(m1.individualid) AS individualid, m1.tagid, m1.taxonid, m1.recordtype, m1.pointid, m1.stemazimuth, m1.stemdistance
+FROM "d0b95d92-3345-4b40-9767-c28ddbbacfae" AS m1
+JOIN (
+  SELECT UPPER(individualid) AS individualid, MAX(date) AS date
+  FROM "d0b95d92-3345-4b40-9767-c28ddbbacfae"
+  WHERE', "siteid IN ({siteList})
+  GROUP BY UPPER(individualid)
+) AS m2
+ON UPPER(m1.individualid) = m2.individualid AND m1.date = m2.date
+ORDER BY UPPER(m1.individualid)", .sep = " ")
+
+#    Get Fulcrum Mapping and Tagging data
+mtDF <- restR2::get.fulcrum.sql(apiToken = fulcrumToken,
+                                sql = mtQuery,
+                                urlEncode = TRUE)
+
+#    Standardize data types and column names
+mtDF <- mtDF %>%
+  dplyr::rename(fixedsubplotid = subplotid,
+                nestedcorner = nestedsubplotid) %>%
+  dplyr::mutate(nestedcorner = as.integer(nestedcorner),
+                recordtype = dplyr::case_when(recordtype %in% c("map and tag", "mapping") ~ "mapAndTag",
+                                              recordtype %in% c("tag only", "tagging") ~ "tagOnly",
+                                              TRUE ~ recordtype),
+                pointid = as.integer(pointid)) %>%
+  dplyr::select(-"date")
 
